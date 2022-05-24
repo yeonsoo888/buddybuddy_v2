67a7db171acdd3c76092328565d75781efe94c38
김연수 <yeonsoo9846@gmail.com>
2022-05-24 23:06:49 +0900

    package 설치

---
+++ app/src/components/header.tsx
@@ -0,0 +1,9 @@
+export default function Header():JSX.Element {
+    return (
+        <header className="header">
+            <h1>
+                <a href="/">budybudy</a>
+            </h1>
+        </header>
+    )
+}
---
+++ app/src/redux/reducer.tsx
@@ -0,0 +1,24 @@
+import { combineReducers } from "redux";
+
+interface MemberState {
+    member: {}
+}
+interface MemberAction {
+    type:string,
+    payload : {},
+}
+const memberReducer = (state:MemberState = {member:{}}, action: MemberAction) => {
+    switch (action.type) {
+        case "loginMember" :
+            return {...state, member:action.payload};
+        case "logoutMember" :
+            return {...state, member: {}};
+        default : 
+            return state;
+    }
+}
+const reducers = combineReducers({
+    memberReducer,
+})
+
+export default reducers
